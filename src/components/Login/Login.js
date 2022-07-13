99e52019cf916494cf2d2283caa383dd5b8572b5
Roman Surkov <s.rb@list.ru>
2022-07-13 11:15:55 +0300

    Использование хука useEffect - запуск кода при изменении состояния зависимостей

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Card from "../UI/Card/Card";
 import styles from "./Login.module.css";
@@ -11,20 +11,18 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    setFormIsValid(
+        inputEmail.includes("@") && inputPassword.trim().length > 7
+    );
+  }, [inputEmail, inputPassword])
+
   const emailChangeHandler = (event) => {
     setInputEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes("@") && inputPassword.trim().length > 7
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setInputPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && inputEmail.includes("@")
-    );
   };
 
   const validateEmailHandler = () => {
