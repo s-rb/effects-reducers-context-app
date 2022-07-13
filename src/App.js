dccce8a7d44ad000e3f56baac7058de7ab9150a3
Roman Surkov <s.rb@list.ru>
2022-07-13 11:07:54 +0300

    Сохранение состояни в локал сторэдж браузера

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
+const isLoggedInKey = 'isLoggedIn';
+const loggedInTrue = '1';
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    setIsLoggedIn(true);
-  };
+    useEffect(() => {
+        const storeLoginInfo = localStorage.getItem(isLoggedInKey);
+        if (storeLoginInfo === loggedInTrue) {
+            setIsLoggedIn(true);
+        }
+    }, [])
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+    const loginHandler = (email, password) => {
+        localStorage.setItem(isLoggedInKey, loggedInTrue);
+        setIsLoggedIn(true);
+    };
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    const logoutHandler = () => {
+        localStorage.removeItem(isLoggedInKey);
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <React.Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler}/>
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
