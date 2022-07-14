8c485fdc39dd3a53ce37c0d7a9102fd030a33fa8
Roman Surkov <s.rb@list.ru>
2022-07-14 19:31:43 +0300

    Использование хука useContext вместо Context.Consumer

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,35 +1,30 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import styles from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+    const ctx = useContext(AuthContext);
     return (
-        <AuthContext.Consumer>
-            {(ctx) => {
-                return (
-                    <nav className={styles.nav}>
-                        <ul>
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Пользователи</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Админ</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <button onClick={props.onLogout}>Выйти</button>
-                                </li>
-                            )}
-                        </ul>
-                    </nav>
-                )
-            }}
-        </AuthContext.Consumer>
+        <nav className={styles.nav}>
+            <ul>
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Пользователи</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Админ</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <button onClick={props.onLogout}>Выйти</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
     );
 };
 
