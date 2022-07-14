b23847d0b136981f5f2cc94296ee183945c89397
Roman Surkov <s.rb@list.ru>
2022-07-14 18:22:14 +0300

    Использование хука useReducers

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,39 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useReducer} from "react";
 
 import Card from "../UI/Card/Card";
 import styles from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+// prevState - последнее состояние. action - результат (параметр) с которым вызывается функция редъюсера (dispathEmailState)
+const emailReducer = (prevState, action) => {
+    if (action.type === 'USER_INPUT') {
+        return {
+            value: action.value,
+            isValid: action.value.includes('@')
+        }
+    } else if (action.type === 'INPUT_BLUR') {
+        return {
+            value: prevState.value,
+            isValid: prevState.value
+        }
+    }
+    return {
+        value: "",
+        isValid: false
+    }
+};
+
 const Login = (props) => {
-    const [inputEmail, setInputEmail] = useState("");
-    const [emailIsValid, setEmailIsValid] = useState();
+    // const [inputEmail, setInputEmail] = useState("");
+    // const [emailIsValid, setEmailIsValid] = useState();
     const [inputPassword, setInputPassword] = useState("");
     const [passwordIsValid, setPasswordIsValid] = useState();
     const [formIsValid, setFormIsValid] = useState(false);
+
+    const [emailState, dispatchEmailState] = useReducer(
+        emailReducer,
+        {value: '', isValid: undefined} // Начальное состояние для emailState
+    );
 
     // Использование useEffect с задержкой, чтобы не на каждое нажатие была реакция, далее чтобы они все не срабатывали
     // сразу, предварительно вызывается функция очистки, в которой здесь удаляется таймер предыдущей итерации
@@ -26,20 +50,20 @@ const Login = (props) => {
      }, [inputEmail, inputPassword])*/
 
     const emailChangeHandler = (event) => {
-        setInputEmail(event.target.value);
+        dispatchEmailState({type: 'USER_INPUT', value: event.target.value}); // вызов этой функции запустит emailReducer
         setFormIsValid(
-            event.target.value.includes("@") && inputPassword.trim().length > 7
+            emailState.isValid && inputPassword.trim().length > 7
         );
     };
 
     const passwordChangeHandler = (event) => {
         setInputPassword(event.target.value);
-        setFormIsValid(event.target.value.trim().length > 7 && inputEmail.includes("@")
+        setFormIsValid(event.target.value.trim().length > 7 && emailState.isValid
         );
     };
 
     const validateEmailHandler = () => {
-        setEmailIsValid(inputEmail.includes("@"));
+        dispatchEmailState({ type: 'INPUT_BLUR' });
     };
 
     const validatePasswordHandler = () => {
@@ -48,7 +72,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(inputEmail, inputPassword);
+        props.onLogin(emailState.value, inputPassword);
     };
 
     return (
@@ -56,14 +80,14 @@ const Login = (props) => {
             <form onSubmit={submitHandler}>
                 <div
                     className={`${styles.control} ${
-                        emailIsValid === false ? styles.invalid : ""
+                        emailState.isValid === false ? styles.invalid : ""
                     }`}
                 >
                     <label htmlFor="email">Email</label>
                     <input
                         type="email"
                         id="email"
-                        value={inputEmail}
+                        value={emailState.value}
                         onChange={emailChangeHandler}
                         onBlur={validateEmailHandler}
                     />
